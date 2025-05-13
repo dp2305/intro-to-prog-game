bbae371b6ccbc5249bf29f3cfdf73975e38e0520
dp2305 <106497046+dp2305@users.noreply.github.com>
2025-05-13 13:24:17 +1000

    updated files

---
+++ Main/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import utils.Game_start;
+
+import static utils.Game_start.*;
+
+public class Main {
+    public static void main(String[] args) {
+        GameStart();
+        backgroundInformation();
+        MissionStart();
+    }
+}
