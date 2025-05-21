bda8e35198771f81125acfa4e287f5192e69948c
dp2305 <106497046+dp2305@users.noreply.github.com>
2025-05-21 14:27:36 +1000

    fixed GameCharacters
    make game character work

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,22 +1,53 @@
 import java.util.Scanner;
-import static utils.Game_start.*;
-import utils.Game_start;
-import utils.Locations;
+import utils.*;
+
 
 public class Main {
 
+    private static Scanner sc = new Scanner(System.in);
+    public static char weaponChosen;
+    private static GameCharacters playerCharacter = new GameCharacters("Dutt", "Human", 50);
+
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        GameStart();
-        backgroundInformation();
-        MissionStart();
+
+        start();
+
+        //backgroundInformation();
+        //MissionStart();
 
         //this is the intro code that gives the plaver the first choice.
         System.out.println("you have been dropped of at the main base.");
         System.out.println("what would you like to do from here");
         System.out.println("(S)earch \n (L)ook around");
-        firstChoice = sc.next().charAt(1);
     }
+
+
+    public static String start() {
+        System.out.println("Choose your name warrior: ");
+        String pName = sc.nextLine();
+        playerCharacter.setName(pName);
+        System.out.println("\nBackground information:");
+
+        return "Game Start";
+    }
+
+
+    public static String backgroundInformation() {
+        System.out.printf("%s you have been chosen for a important reconnaissance mission. \nyou objective is to rescue scientists that were conducting important reseach.",playerCharacter.getName());
+        System.out.println("\nAlong the way you will find other squads which were deployed earlier but failed to finish the task");
+        System.out.println("God Speed soldier.");
+        return "";
+    }
+
+    public static String MissionStart() {
+        System.out.println("\nChoose a weapon to start with");
+        System.out.println("(K)nife, (P)istol, (R)ifle");
+        weaponChosen  = sc.next().charAt(0);
+        System.out.println("you are inside of a helicopter, about to be dropping into the mission area. ");
+        return "";
+    }
+
+
 }
