626fedcd987cc6ab34c5ba278cfe107387873df2
ida5428 <adiv5428@gmail.com>
2025-05-25 00:04:57 +1000

    Updated weapon selection, added more enemeis attacks, and added backpack with item addition and removal

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -17,13 +17,17 @@ public class Main {
 
     // Game variables
     private static Player playerCharacter = new Player("Player", 50);
+    public static boolean gameRunning = true;
     private static boolean validChoiceHandler;
+    private static boolean validChoiceHandler2;
 
     public static void main(String[] args) {
         startTutorial();
         startGame();
         displayBackgroundInformation();
-        missionStart();
+        missionStartPart1();
+
+        print(playerCharacter.getWeapon().getName());
 
         // // Handle player's choice
         // while (validChoiceHandler) {
@@ -57,6 +61,7 @@ public class Main {
         // }
     }
 
+    // Method to show player what colours coresponde to what
     public static void startTutorial() {
         print(ANSI_TEXT_GREEN + "Green" + ANSI_RESET + " means that you must enter your choice, for example:");
         lineBreak();
@@ -64,49 +69,60 @@ public class Main {
         lineBreak();
         print(" - " + ANSI_TEXT_GREEN + " > " + ANSI_RESET + "Y");
         lineBreak();
+        print(ANSI_TEXT_BLUE + "Blue" + ANSI_RESET + " are the choices that you can make.");
+        lineBreak();
         print(ANSI_TEXT_YELLOW + "Yellow" + ANSI_RESET + " is information about the story of the game.");
         lineBreak();
         print(ANSI_TEXT_RED + "Red" + ANSI_RESET + " is an error message.");
         lineBreak();
-        print(ANSI_TEXT_BLUE + "Blue" + ANSI_RESET + " are the choices that you can make.");
         lineBreak();
     }
 
     // Method for starting the game
+    // This is where the player is asked to enter their name
     public static void startGame() {
-        lineBreak();
-        printColour("      -----== Game Start ==-----", ANSI_TEXT_YELLOW);
-        lineBreak();
-        lineBreak();
-
         validChoiceHandler = true;
 
+        printColour("            -----=== Game Start ===-----", ANSI_TEXT_YELLOW);
+        lineBreak();
+        lineBreak();
+
         while (validChoiceHandler) {
+            validChoiceHandler2 = true;
             printColour("Enter your name: ", ANSI_TEXT_GREEN);
             String playerName = sc.nextLine();
 
+            // If the player enters an empty name, the line is cleared
             if (playerName.isEmpty()) {
-                printColour("Invalid name. Please try again.", ANSI_TEXT_RED);
-                lineBreak();
+                clearLine(1);
             } else {
-                print("Are you sure you want to use \"" + ANSI_TEXT_YELLOW + playerName + ANSI_RESET + "\" as your name?" + ANSI_TEXT_BLUE + " (Y)es / (N)o" + ANSI_RESET);
-                lineBreak();
-                printColour(" > ", ANSI_TEXT_GREEN);
+                // Asks the player to confirm their name
+                while (validChoiceHandler2) {
+                    print("Are you sure you want to use \"" + ANSI_TEXT_YELLOW + playerName + ANSI_RESET + "\" as your name?" + ANSI_TEXT_BLUE + " (Y)es / (N)o" + ANSI_RESET);
+                    lineBreak();
+                    printColour(" > ", ANSI_TEXT_GREEN);
+                    String nameConfirm = sc.nextLine().toUpperCase();
 
-                char nameConfirm = sc.next().toUpperCase().charAt(0);
-                sc.nextLine();
+                    if (nameConfirm.isEmpty()) {
+                        clearLine(2);
+                    } else {
+                        char nameConfirmChar = nameConfirm.charAt(0);
 
-                switch (nameConfirm) {
-                    case 'Y' -> {
-                        playerCharacter.setName(playerName);
-                        validChoiceHandler = false;
-                        lineBreak();
-                    }
-                    case 'N' -> {
-                    }
-                    default -> {
-                        printColour("Invalid choice. Please try again.", ANSI_TEXT_RED);
-                        lineBreak();
+                        switch (nameConfirmChar) {
+                            case 'Y' -> {
+                            playerCharacter.setName(playerName);
+                            validChoiceHandler = false;
+                            validChoiceHandler2 = false;
+                            lineBreak();
+                            }
+                            case 'N' -> {
+                                clearLine(3);
+                                validChoiceHandler2 = false;
+                            }
+                            default -> {
+                                clearLine(2);
+                            }
+                        }
                     }
                 }
             }
@@ -115,76 +131,91 @@ public class Main {
 
     // Method for displaying the background information
     public static void displayBackgroundInformation() {
-        print(ANSI_TEXT_YELLOW + playerCharacter.getName() + ANSI_RESET + ", you have been chosen for an important reconnaissance mission.\nYour objective is to rescue scientists that were conducting important research.");
+        print("\"" + ANSI_TEXT_YELLOW + playerCharacter.getName() + ANSI_RESET + ", you have been chosen for an important reconnaissance mission.");
         lineBreak();
-        print("Along the way you will find other squads which were deployed earlier but failed to finish the task");
+        print("Your objective is to rescue scientists that were conducting important research at a classified facility.");
         lineBreak();
-        print("Godspeed soldier.");
+        print("Our teams haven't been able to find any information about the facility, but we know that it is highly dangerous.");
+        lineBreak();
+        print("Godspeed soldier.\"");
         lineBreak();
         lineBreak();
     }
 
     // Method for starting the mission
-    public static void missionStart() {
-        int errorCount = 0;
+    // This is where the player is asked to choose a weapon
+    public static void missionStartPart1() {
         validChoiceHandler = true;
 
         print("Choose a weapon to start with: ");
         printColour("(K)nife, (P)istol, (R)ifle", ANSI_TEXT_BLUE);
         lineBreak();
-        print("The " + ANSI_TEXT_BLUE + "Knife" + ANSI_RESET + " is the most basic weapon, it has" + ANSI_TEXT_YELLOW + " low damage and range" + ANSI_RESET + " but its" + ANSI_TEXT_YELLOW + " the lightest, making it agile." + ANSI_RESET + "." + "\n");
+        print("The " + ANSI_TEXT_BLUE + "Knife" + ANSI_RESET + " is the most basic weapon, it has" + ANSI_TEXT_YELLOW + " low damage and range" + ANSI_RESET + " but its" + ANSI_TEXT_YELLOW + " the lightest, making it agile." + ANSI_RESET + ".");
         lineBreak();
-        print("The " +ANSI_TEXT_BLUE +  "Pistol" +ANSI_RESET + " has a larger range with a moderate damage output, but, its heavier than the knife and doesn't come with any ammo."+ "\n");
+        print("The " + ANSI_TEXT_BLUE + "Pistol" + ANSI_RESET + " has a larger range with a moderate damage output, but, its heavier than the knife and doesn't come with any ammo.");
         lineBreak();
-        print("The " +ANSI_TEXT_BLUE+ "Rifle" +ANSI_RESET + " is a" + ANSI_TEXT_YELLOW + " long range weapon with a high damage output" + ANSI_RESET + " but it is" + ANSI_TEXT_YELLOW + " heavy and difficult to handle" + ANSI_RESET + "."+ "\n");
+        print("The " + ANSI_TEXT_BLUE + "Rifle" + ANSI_RESET + " is a" + ANSI_TEXT_YELLOW + " long range weapon with a high damage output" + ANSI_RESET + " but it is" + ANSI_TEXT_YELLOW + " heavy and difficult to handle" + ANSI_RESET + ".");
         lineBreak();
+
         while (validChoiceHandler) {
+            validChoiceHandler2 = true;
             printColour(" > ", ANSI_TEXT_GREEN);
-            char weaponChosen = sc.next().toUpperCase().charAt(0);
-            sc.nextLine();
-            switch (weaponChosen) {
-                case 'K' -> {
-                    playerCharacter.setWeapon(new Weapon("Knife", 3, 1000000, 1, 1));
-                }
-                case 'P' -> {
-                    playerCharacter.setWeapon(new Weapon("Pistol", 6, 0, 2, 2));
-                }
-                case 'R' -> {
-                    playerCharacter.setWeapon(new Weapon("Rifle", 15, 12, 3, 3));
-                }
-                default -> {
+            String weaponChosen = sc.nextLine().toUpperCase();
 
-                    printColour("Invalid choice. Please try again.", ANSI_TEXT_RED);
-                    lineBreak();
-                    continue;
-                }
-            }
+            if (weaponChosen.isEmpty()) {
+                clearLine(1);
+            } else {
+                char weaponChosenChar = weaponChosen.charAt(0);
 
-            print("Are you sure you want to use \"" + ANSI_TEXT_YELLOW + playerCharacter.getWeapon().getName() + ANSI_RESET + "\" as your weapon?" + ANSI_TEXT_BLUE + " (Y)es / (N)o" + ANSI_RESET);
-            lineBreak();
-            printColour(" > ", ANSI_TEXT_GREEN);
-            char weaponConfirm = sc.next().toUpperCase().charAt(0);
-            sc.nextLine();
-
-            switch (weaponConfirm) {
-                case 'Y' -> {
-                    lineBreak();
-                    validChoiceHandler = false;
-                }
-                case 'N' -> {
-                }
-                default -> {
-                    for (int i = 0; i < 5; i++) {
-                        print("\033[1A\033[2K");
+                switch (weaponChosenChar) {
+                    case 'K' -> {
+                        playerCharacter.setWeapon(new Weapon("Knife", 3, 1000000, 1, 1));
                     }
-                    printColour("Invalid choice. Please try again.", ANSI_TEXT_RED);
+                    case 'P' -> {
+                        playerCharacter.setWeapon(new Weapon("Pistol", 6, 0, 2, 2));
+                    }
+                    case 'R' -> {
+                        playerCharacter.setWeapon(new Weapon("Rifle", 15, 12, 3, 3));
+                    }
+                    default -> {
+                        clearLine(1);
+                        continue;
+                    }
+                }
+
+                while (validChoiceHandler2) {
+                    print("Are you sure you want to use the " + ANSI_TEXT_YELLOW + playerCharacter.getWeapon().getName() + ANSI_RESET + " as your weapon?" + ANSI_TEXT_BLUE + " (Y)es / (N)o" + ANSI_RESET);
                     lineBreak();
+                    printColour(" > ", ANSI_TEXT_GREEN);
+                    String weaponConfirm = sc.nextLine().toUpperCase();
+
+                    if (weaponConfirm.isEmpty()) {
+                        clearLine(2);
+                    } else {
+                        char weaponConfirmChar = weaponConfirm.charAt(0);
+
+                        switch (weaponConfirmChar) {
+                            case 'Y' -> {
+                                validChoiceHandler = false;
+                                validChoiceHandler2 = false;
+                            }
+                            case 'N' -> {
+                                clearLine(3);
+                                validChoiceHandler2 = false;
+                            }
+                            default -> {
+                                clearLine(2);
+                            }
+                        }
+                    }
                 }
             }
         }
+    }
 
-        lineBreak();
-
+    // Method for starting the mission
+    // This is where the player is asked to choose 2 items to start with
+    public static void missionStartPart2() {
         print("Choose 2 items to start with: ");
         printColour("1. Food Pack, 2. Ammo Box, 3. First Aid Kit", ANSI_TEXT_BLUE);
         lineBreak();
@@ -217,5 +248,12 @@ public class Main {
     // Method to print a line break
     public static void lineBreak() {
         System.out.println();
+    }
+
+    // Method to clear line(s) for a cleaner console output, e.g. after an error message is printed
+    public static void clearLine(int clearLineCount) {
+        for (int i = 0; i < clearLineCount; i++) {
+            System.out.print("\033[1A\033[2K");
+        }
     }
 }
